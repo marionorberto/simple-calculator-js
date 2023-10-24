8f6064e70dd13299c0c1330ab07b8601436baea3
Mário Norberto <marionorberto2018@gmail.com>
2023-10-24 20:04:52 +0100

    initial

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+const display = document.querySelector('.display');
+
+document.addEventListener('click', (e) => {
+    
+    if(e.target.classList.contains('btn')) fillInput(e.target);
+    if(e.target.classList.contains('AC')) clearInput();
+    if(e.target.classList.contains('DEL')) deleteOne();
+    if(e.target.classList.contains('pi')) insertPiValue();
+    if(e.target.classList.contains('equal')) solve();
+});
+
+const fillInput = (elem) => {
+    display.value += elem.innerText;
+}
+
+const clearInput = () => display.value = '';
+const deleteOne = () => display.value = display.value.slice(0, -1);
+const insertPiValue = () => display.value += Math.PI;
+
+const solve = () => {
+    console.log('...');
+}
