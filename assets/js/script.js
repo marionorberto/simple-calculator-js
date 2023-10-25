3e27fd5c9a18d8215b6ea04703372c8a80486e47
Mário Norberto <marionorberto2018@gmail.com>
2023-10-25 13:08:34 +0100

    finished design

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,15 @@
 const display = document.querySelector('.display');
 
 document.addEventListener('click', (e) => {
-    
     if(e.target.classList.contains('btn')) fillInput(e.target);
+    if(display.value.length <= 15) {
+        display.style= 'font-size: 25px;'; 
+    } if (display.value.length > 15) {
+        display.style= 'font-size: 22px;'; 
+    } if(display.value.length > 20) {
+        display.style= 'font-size: 18px;'; 
+    }
+        
     if(e.target.classList.contains('AC')) clearInput();
     if(e.target.classList.contains('DEL')) deleteOne();
     if(e.target.classList.contains('pi')) insertPiValue();
@@ -18,5 +25,6 @@ const deleteOne = () => display.value = display.value.slice(0, -1);
 const insertPiValue = () => display.value += Math.PI;
 
 const solve = () => {
-    console.log('...');
+    return console.log('... missing something!!!');
 }
+
